b3cf08467525e4397bf2ded003ce20d821a85b56
JayaLakshmi Pathi <jayalakshmipathi215@gmail.com>
2025-07-17 18:59:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// Typed.js animation
+var typed = new Typed(".text", {
+  strings: ["Software Developer", "Frontend Developer", "Python Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// Hamburger Menu
+const menuIcon = document.getElementById("menu-icon");
+const navMenu = document.getElementById("nav-menu");
+
+menuIcon.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
